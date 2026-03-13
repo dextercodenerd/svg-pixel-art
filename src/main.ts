a27b00522fe44ed64a94fa123b94b7a1732daab0
Dexter <dexter.codenerd@gmail.com>
2026-03-13 20:51:06 +0100

    Add license and copyright

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+/**
+ * Copyright (C) 2026 Dexter
+ *
+ * This source code is licensed under the GNU Affero General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
--- tests/palette.test.ts
+++ tests/palette.test.ts
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+/**
+ * Copyright (C) 2026 Dexter
+ *
+ * This source code is licensed under the GNU Affero General Public License v3.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
 import { nextTick } from 'vue'
